9dad7a497d548aa4bcf9208754356b23a105fa13
JensonWagner-Clemson <jensonw@clemson.edu>
2025-10-10 22:04:44 -0400

    fixed error with two server.js files and now able to post data to db

--- backend/admin-service/server.js
+++ backend/admin-service/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const adminRoutes = require('./routes/adminRoute');
+const adminRoute = require('./routes/adminRoute');
 
 const app = express();
 
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json()); //Middleware to parse JSON bodies
 
 //Routes
-app.use('/api/admin', adminRoutes);
+app.use('/api/admin', adminRoute);
 
 const PORT = 5001;
 app.listen(PORT, () => console.log(`Admin service running at http://localhost:${PORT}`));
