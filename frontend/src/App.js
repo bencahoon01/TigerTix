f515e3fd4a212d4ff2ab4c471a126b28608dcd4a
Tyler Fry <thfry@clemson.edu>
2025-10-11 17:27:36 -0400

    changed the fetch to 6001

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/events')
+    fetch('http://localhost:6000/api/events')
       .then((res) => res.json())
       .then((data) => setEvents(data))
       .catch((err) => console.error(err));
